da70a676c4725762e71fa3f398925633af3fe07b
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2024-05-31 11:50:09 -0600

    Haciendo account

--- src/account/account.controller.js
+++ src/account/account.controller.js
@@ -23,12 +23,23 @@ export const saveAccount = async(req, res) =>{
         if(!user) return res.status(404).send({message: 'User not found'})
         let findAccount = await Account.findOne({user: data.user, typeofaccount: data.typeofaccount})
         if(findAccount) return res.status(401).send({message: 'Account alredy exists'})
+        if(data.balance < 0 ) return res.status(401).send({message: 'The balance cannot be less than 0'})
         data.noaccount = await mathRandom()
         let account = new Account(data)
         await account.save()
-        return res.send({message: 'Account save successfully'})
+        return res.send({message: 'Account save successfully', account})
     } catch (err) {
         console.error(err)
         return res.status(500).send({message: 'Error save account'})
+    }
+}
+
+export const getAccount = async(req, res) =>{
+    try {
+        let account = await Account.find()
+        return res.send(account)
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({message: 'Error getting account'})
     }
 }
--- src/account/account.routes.js
+++ src/account/account.routes.js
@@ -1,12 +1,13 @@
 'use strict'
 
 import { Router } from "express";
-import { saveAccount, test } from './account.controller.js'
+import { getAccount, saveAccount, test } from './account.controller.js'
 import { isAdmin, validateJwt } from '../middlewares/validate_Jwt.js'
 
 const api = Router()
 
 api.get('/test', test)
+api.get('/getAccount', [validateJwt, isAdmin], getAccount)
 api.post('/saveAccount', [validateJwt, isAdmin], saveAccount)
 
 export default api
