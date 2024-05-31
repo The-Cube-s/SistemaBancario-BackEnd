2feda57dbe609f08da6f03de34872c7cc03399c2
dbarcarcel-2020380 <dbarcarcel-2020380@kinal.edu.gt>
2024-05-31 10:20:28 -0600

    Validar la vaina del monthly

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,174 +1,174 @@
-'use strict'
+'use strict';
 
-import User from './user.model.js'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
-import { encrypt, checkPassword, checkUpdate } from '../utils/validator.js'
-import { generateJwt } from '../utils/jwt.js'
+import User from './user.model.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { encrypt, checkPassword, checkUpdate } from '../utils/validator.js';
+import { generateJwt } from '../utils/jwt.js';
 
-export const test = (req, res) =>{
+export const test = (req, res) => {
     console.log('Test is running');
-    return res.send({message: 'Test is running'})
-}
+    return res.send({ message: 'Test is running' });
+};
 
-export const login = async(req, res) => {
+export const login = async (req, res) => {
     try {
-        let {account, password} = req.body
+        let { account, password } = req.body;
         let user = await User.findOne({
             $or: [
-                {username: account},
-                {email: account}
+                { username: account },
+                { email: account }
             ]
-        })
-       if(user && await checkPassword(password, user.password)){
+        });
+        if (user && await checkPassword(password, user.password)) {
             let loggedUser = {
                 uid: user._id,
                 name: user.name,
                 username: user.username,
                 role: user.role
-            }
-            let token = await generateJwt(loggedUser)
-            return res.send(
-                {message: `Welcome ${loggedUser.name}`
-                , loggedUser, token})
+            };
+            let token = await generateJwt(loggedUser);
+            return res.send({
+                message: `Welcome ${loggedUser.name}`,
+                loggedUser,
+                token
+            });
         }
-        return res.status(400).send({message: `User not found`})
+        return res.status(400).send({ message: `User not found` });
     } catch (err) {
         console.error(err);
-        return res.status(500).send({message: 'Error to login'})
+        return res.status(500).send({ message: 'Error to login' });
     }
-}
+};
 
-
-const mathRandom = () =>{
-    let num = ''
-    for(let i = 0; i < 20; i++){
-        num += Math.floor(Math.random() * 10)
+const mathRandom = () => {
+    let num = '';
+    for (let i = 0; i < 20; i++) {
+        num += Math.floor(Math.random() * 10);
     }
-    return num
-}
+    return num;
+};
 
-
-export const userDefect = async(req,res) =>{
+export const userDefect = async (req, res) => {
     try {
-        const userExists = await User.findOne({username: 'ADMINB'})        
-        if(userExists){
-           console.log('usuario existente')
-        }else{
-        const encryptPassword = await encrypt('ADMINB')
-        const newUser = new User({
-            name: 'ADMINB',
-            surname: 'ADMINB',
-            username: 'ADMINB',
-            password: encryptPassword,
-            noaccount: mathRandom(),
-            DPI: '1234567891234',
-            address: 'ADMINB',
-            email: 'ADMINB',
-            phone: '12345678',
-            role: 'ADMIN',
-            jobname: 'asdasd',
-            monthlyincome: '00.00'
-        })
-         
-        await newUser.save()
-    }   
+        const userExists = await User.findOne({ username: 'ADMINB' });
+
+        if (userExists) {
+            console.log('usuario existente');
+        } else {
+            const encryptPassword = await encrypt('ADMINB');
+            const newUser = new User({
+                name: 'ADMINB',
+                surname: 'ADMINB',
+                username: 'ADMINB',
+                password: encryptPassword,
+                noaccount: mathRandom(),
+                DPI: '1234567891234',
+                address: 'ADMINB',
+                email: 'ADMINB',
+                phone: '12345678',
+                role: 'ADMIN',
+                jobname: 'asdasd',
+                monthlyincome: '00.00'
+            });
+
+            // Validar que monthlyincome sea mayor o igual a 100
+            if (parseFloat(newUser.monthlyincome) < 100) {
+                //return res.status(400).send({ message: 'Monthly income should be 100 or more' });
+                return res.status(400).send({ message: 'Error register user'});
+            }
+
+            await newUser.save();
+        }
     } catch (err) {
-        console.error(err)
+        console.error(err);
+        return res.status(500)
     }
-} 
+};
 
-//Register general 
-export const register = async(req, res) =>{
+export const register = async (req, res) => {
     try {
-        let data = req.body
+        let data = req.body;
         let findUser = await User.findOne({
             $or: [
-                {username: data.username},
-                {email: data.email}
+                { username: data.username },
+                { email: data.email }
             ]
-        })
-        if(findUser) return res.status(403).send({message: `User ${data.username} alredy exists`})
-        data.password = await encrypt(data.password) 
-        data.noaccount = await mathRandom()
-        let user = new User(data)
-        await user.save()
-        return res.send({message: `Register successfully, can be logged with user ${user.username}`})
+        });
+        if (findUser) return res.status(403).send({ message: `User ${data.username} already exists` });
+        data.password = await encrypt(data.password);
+        data.noaccount = await mathRandom();
+        let user = new User(data);
+        await user.save();
+        return res.send({ message: `Register successfully, can be logged with user ${user.username}` });
     } catch (err) {
         console.error(err);
-        return res.status(500).send({message: 'Error register user', err })
+        return res.status(500).send({ message: 'Error register user', err });
     }
-}
+};
 
-export const updateAdmin = async(req, res) => {
+export const updateAdmin = async (req, res) => {
     try {
-        let data = req.body
-        let { id } = req.params
-        let update = checkUpdate(data, id)
-        if(!update) return res.status(400).send({message: 'Have submitted some data that can not be update'})
-        let secretKey = process.env.SECRET_KEY
-        let token = req.headers.authorization
-        const { uid } = jwt.verify(token, secretKey)
-        if( uid != id) return res.status(404).send({message: 'You cant not update another ADMIN'})
-        let updateAdmin = await User.findOneAndUpdate(
-            {_id: id},
-            data,
-            {new: true}
-        )
-        if(!updateAdmin) return res.status(404).send({message: 'ADMIN not found and not update'})
-        return res.send({message: 'ADMIN updated successfully', updateAdmin})
-    } catch (err) {
-        console.error(err);
-        return res.status(500).send({message: 'Error updating ADMIN'})
-    }
-}
-
-export const update = async(req, res) =>{
-    try {
-        let { id } = req.params
-        let data = req.body
-        let update = checkUpdate(data, id)
-        if(!update) return res.status(400).send({message: 'Have submitted some data that can not be update'})
-        data.password = await encrypt(data.password)
+        let data = req.body;
+        let { id } = req.params;
+        let update = checkUpdate(data, id);
+        if (!update) return res.status(400).send({ message: 'Have submitted some data that cannot be updated' });
+        let secretKey = process.env.SECRET_KEY;
+        let token = req.headers.authorization;
+        const { uid } = jwt.verify(token, secretKey);
+        if (uid != id) return res.status(404).send({ message: 'You cannot update another ADMIN' });
         let updateUser = await User.findOneAndUpdate(
             { _id: id },
             data,
             { new: true }
-        )
-        if(!updateUser) return res.status(401).send({message: 'User not fount and not update'})
-        return res.send({message: 'Update user', updateUser})
-    } catch (err) {
-        console.error(err)
-        if(err.keyValue.username)return res.status(400).send({message: `Username ${err.keyValue.username} is alredy exists`})
-        return res.status(500).send({message: 'Error updating account'})
-    }
-}
-
-export const deleteUser = async(req, res)=>{
-    try {
-        let { id } = req.params
-        let deletedUser = await User.findOneAndDelete({_id: id})
-        if(!deletedUser) return res.status(404).send({message: 'Account not found and not delete'})
-        return res.send({message: `Account with username ${deletedUser.username} delete successfully`}) 
+        );
+        if (updateUser === undefined) return res.status(401).send({ message: 'User not found and not updated' });
+        return res.send({ message: 'ADMIN updated successfully', updateUser });
     } catch (err) {
         console.error(err);
-        return res.status(500).send({message: `Error deleting account`})
+        return res.status(500).send({ message: 'Error updating ADMIN' });
     }
-}
+};
 
-export const getUser = async(req, res) => {
+export const update = async (updateUser, req, res) => {
     try {
-        let users = await User.find()
-        return res.send({users})
+        let { id } = req.params;
+        let data = req.body;
+        let update = checkUpdate(data, id);
+        if (!update) return res.status(400).send({ message: 'Have submitted some data that cannot be updated' });
+        data.password = await encrypt(data.password);
+        let updateUser = await User.findOneAndUpdate(
+            { _id: id },
+            data,
+            { new: true }
+        );
+        if (!updateUser) return res.status(401).send({ message: 'User not found and not updated' });
+        return res.send({ message: 'Update user', updateUser });
     } catch (err) {
         console.error(err);
-        return res.status(500).send({message: 'Error getting users'})
+        if (err.keyValue.username) return res.status(400).send({ message: `Username ${err.keyValue.username} already exists` });
+        return res.status(500).send({ message: 'Error updating account' });
     }
-}
+};
 
-/* FUNCIONES DEL ADMIN AL CLIENTE 
-    EDITAR, ELIMINAR, VISUALIZAR, AGREGAR OTRO ADMIN o CLIENTE
+export const deleteUser = async (req, res) => {
+    try {
+        let { id } = req.params;
+        let deletedUser = await User.findOneAndDelete({ _id: id });
+        if (!deletedUser) return res.status(404).send({ message: 'Account not found and not deleted' });
+        return res.send({ message: `Account with username ${deletedUser.username} deleted successfully` });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({ message: `Error deleting account` });
+    }
+};
 
-    EXCEPTO A OTRO ADMIN
-*/
-
+export const getUser = async (req, res) => {
+    try {
+        let users = await User.find();
+        return res.send({ users });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({ message: 'Error getting users' });
+    }
+};
