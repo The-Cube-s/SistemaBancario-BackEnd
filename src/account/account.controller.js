1a35764599c3abad83a8dc8c7ac0a2b2e25380ad
dbarcarcel-2020380 <dbarcarcel-2020380@kinal.edu.gt>
2024-06-15 15:28:13 -0600

    La vaina que dijo Pampichi

--- src/account/account.controller.js
+++ src/account/account.controller.js
@@ -2,6 +2,7 @@
 
 import Account from './account.model.js';
 import User from '../user/user.model.js';
+import axios from "axios";
 
 const mathRandom = () => {
     let num = '';
@@ -21,23 +22,34 @@ export const saveAccount = async (req, res) => {
         let data = req.body;
         let user = await User.findOne({ _id: data.user });
         if (!user) return res.status(404).send({ message: 'User not found' });
-        let findAccount = await Account.findOne({ user: data.user, typeofaccount: data.typeofaccount });
-        if (findAccount) return res.status(401).send({ message: 'Account already exists' });
+
+        // Verificar si ya tiene una cuenta del tipo especificado
+        let existingAccounts = await Account.find({ user: data.user });
+
+        let hasMonetaria = existingAccounts.some(account => account.typeofaccount === 'MONETARIA');
+        let hasAhorro = existingAccounts.some(account => account.typeofaccount === 'AHORRO');
+
+        if ((data.typeofaccount === 'MONETARIA' && hasMonetaria) || (data.typeofaccount === 'AHORRO' && hasAhorro)) {
+            return res.status(401).send({ message: `This user only can has 1 ${data.typeofaccount.toLowerCase()} account` });
+        }
+
         if (data.balance < 0) return res.status(401).send({ message: 'The balance cannot be less than 0' });
+
         data.noaccount = await mathRandom();
         let account = new Account(data);
         await account.save();
-        return res.send({ message: 'Account save successfully', account });
+        return res.send({ message: 'Account saved successfully', account });
     } catch (err) {
         console.error(err);
-        return res.status(500).send({ message: 'Error save account' });
+        return res.status(500).send({ message: 'Error saving account' });
     }
 };
 
 export const getAccount = async (req, res) => {
     try {
-        let account = await Account.find();
-        return res.send(account);
+        let accounts = await Account.find();
+        //desestructure
+        return res.send({accounts});
     } catch (err) {
         console.error(err);
         return res.status(500).send({ message: 'Error getting account' });
@@ -49,18 +61,25 @@ export const getAccountBalance = async (req, res) => {
         const accountId = req.params.id;
         const account = await Account.findById(accountId);
 
+        // Si el número de cuenta no existe
         if (!account) {
             return res.status(404).send({ message: 'Account not found' });
         }
 
         const user = await User.findById(account.user);
 
+        // Si el usuario no existe
         if (!user) {
             return res.status(404).send({ message: 'User not found' });
         }
 
-        // Assuming req.user contains the authenticated user details
-        if (req.user.role === 'admin' || req.user._id.toString() === user._id.toString()) {
+        // Si el usuario existe pero no tiene ese tipo de cuenta
+        if (account.typeofaccount !== req.body.typeofaccount) {
+            return res.status(400).send({ message: `User does not have an account of type ${req.body.typeofaccount}` });
+        }
+
+        // Verificando si el usuario autenticado es ADMIN o el propietario de la cuenta
+        if (req.user.role === 'ADMIN' || req.user._id.toString() === user._id.toString()) {
             return res.send({ balance: account.balance });
         } else {
             return res.status(403).send({ message: 'Access denied' });
@@ -70,3 +89,36 @@ export const getAccountBalance = async (req, res) => {
         return res.status(500).send({ message: 'Error getting account balance' });
     }
 };
+
+
+export const convertCurrency = async (req, res) => {
+    const path = process.env.API_URL;
+    const key = process.env.API_KEY;
+    const { from, to } = req.body;
+    const userId = req.user._id; // Obtener el ID del usuario autenticado
+
+    try {
+        // Buscar la cuenta más reciente del usuario
+        const account = await Account.findOne({ user: userId }).sort({ _id: -1 });
+        if (!account) return res.status(404).send({ message: 'Account not found' });
+
+        const amount = account.balance;
+        const url = `${path}/${key}/pair/${from}/${to}/${amount}`;
+
+        const { data } = await axios.get(url);
+
+        if (data && data.result === 'success') {
+            return res.send({
+                from: from,
+                to: to,
+                conversionRate: data.conversion_rate,
+                convertedAmount: data.conversion_result
+            });
+        }
+
+        return res.status(418).send({ message: 'Error en la solicitud', data });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ message: 'Error al realizar la conversion', error });
+    }
+}
